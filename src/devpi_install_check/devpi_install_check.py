521d77c98c305319776f620ebd30c8aec4909cfd
Florian Schulze <mail@florian-schulze.net>
2020-05-04 10:39:52 +0200

    Updates for Python 3.

--- src/devpi_install_check/devpi_install_check.py
+++ src/devpi_install_check/devpi_install_check.py
@@ -1,4 +1,4 @@
-from ConfigParser import RawConfigParser
+from configparser import RawConfigParser
 import os
 import sys
 
@@ -14,74 +14,74 @@ def check_setuptools(host, port, url):
     path = os.path.expanduser('~/.pydistutils.cfg')
     failure = False
     if not os.path.exists(path):
-        print "~/.pydistutils.cfg doesn't exist."
+        print("~/.pydistutils.cfg doesn't exist.")
     else:
         config = read_config(path)
         if config.has_option('easy_install', 'index_url'):
             value = config.get('easy_install', 'index_url')
             if not value.startswith(url):
-                print "ERROR: The index_url '%s' in the easy_install section of ~/.pydistutils.cfg doesn't start with '%s'." % (value, url)
+                print("ERROR: The index_url '%s' in the easy_install section of ~/.pydistutils.cfg doesn't start with '%s'." % (value, url))
                 failure = True
         if config.has_option('easy_install', 'allow_hosts'):
             value = config.get('easy_install', 'allow_hosts')
             if host not in value:
-                print "ERROR: The allow_hosts setting '%s' in the easy_install section of ~/.pydistutils.cfg doesn't contain '%s'." % (value, host)
+                print("ERROR: The allow_hosts setting '%s' in the easy_install section of ~/.pydistutils.cfg doesn't contain '%s'." % (value, host))
                 failure = True
     if not os.path.exists(path) or (not config.has_option('easy_install', 'index_url') and not failure):
-        print "If you want to let setuptools use this devpi server by default, add the following section to ~/.pydistutils.cfg."
-        print "[easy_install]"
-        print "index_url = %s/[devpi user name]/[index name]/+simple/" % url
-        print "allow_hosts = %s" % host
+        print("If you want to let setuptools use this devpi server by default, add the following section to ~/.pydistutils.cfg.")
+        print("[easy_install]")
+        print("index_url = %s/[devpi user name]/[index name]/+simple/" % url)
+        print("allow_hosts = %s" % host)
     else:
-        print "~/.pydistutils.cfg seems to be fine."
-    print
+        print("~/.pydistutils.cfg seems to be fine.")
+    print()
 
 
 def check_pip(host, port, url):
     path = os.path.expanduser('~/.pip/pip.conf')
     failure = False
     if not os.path.exists(path):
-        print "~/.pip/pip.conf doesn't exist."
+        print("~/.pip/pip.conf doesn't exist.")
     else:
         config = read_config(path)
         if config.has_option('global', 'index-url'):
             value = config.get('global', 'index-url')
             if not value.startswith(url):
-                print "ERROR: The index-url '%s' in the global section of ~/.pip/pip.conf doesn't start with '%s'." % (value, url)
+                print("ERROR: The index-url '%s' in the global section of ~/.pip/pip.conf doesn't start with '%s'." % (value, url))
                 failure = True
         if config.has_option('search', 'index'):
             value = config.get('search', 'index')
             if not value.startswith(url):
-                print "ERROR: The index '%s' in the search section of ~/.pip/pip.conf doesn't start with '%s'." % (value, url)
+                print("ERROR: The index '%s' in the search section of ~/.pip/pip.conf doesn't start with '%s'." % (value, url))
                 failure = True
     if not os.path.exists(path) or (not config.has_option('global', 'index-url') and not failure):
-        print "If you want to let pip use this devpi server by default, add the following section to ~/.pip/pip.conf."
-        print "[global]"
-        print "index-url = %s/[devpi user name]/[index name]/+simple/" % url
+        print("If you want to let pip use this devpi server by default, add the following section to ~/.pip/pip.conf.")
+        print("[global]")
+        print("index-url = %s/[devpi user name]/[index name]/+simple/" % url)
     else:
-        print "~/.pip/pip.conf seems to be fine."
-    print
+        print("~/.pip/pip.conf seems to be fine.")
+    print()
 
 
 def check_buildout(host, port, url):
     path = os.path.expanduser('~/.buildout/default.cfg')
     failure = False
     if not os.path.exists(path):
-        print "~/.buildout/default.cfg doesn't exist."
+        print("~/.buildout/default.cfg doesn't exist.")
     else:
         config = read_config(path)
         if config.has_option('buildout', 'index'):
             value = config.get('buildout', 'index')
             if not value.startswith(url):
-                print "ERROR: The index '%s' in the buildout section of ~/.buildout/default.cfg doesn't start with '%s'." % (value, url)
+                print("ERROR: The index '%s' in the buildout section of ~/.buildout/default.cfg doesn't start with '%s'." % (value, url))
                 failure = True
     if not os.path.exists(path) or (not config.has_option('buildout', 'index') and not failure):
-        print "If you want to let pip use this devpi server by default, add the following section to ~/.buildout/default.cfg."
-        print "[buildout]"
-        print "index = %s/[devpi user name]/[index name]/+simple/" % url
+        print("If you want to let pip use this devpi server by default, add the following section to ~/.buildout/default.cfg.")
+        print("[buildout]")
+        print("index = %s/[devpi user name]/[index name]/+simple/" % url)
     else:
-        print "~/.buildout/default.cfg seems to be fine."
-    print
+        print("~/.buildout/default.cfg seems to be fine.")
+    print()
 
 
 def check_launch_agent(base):
@@ -90,17 +90,17 @@ def check_launch_agent(base):
         src = os.path.join(base, 'etc', 'devpi-server.plist')
         link = "ln -s %s %s" % (src, dest)
         if not os.path.exists(dest):
-            print "WARNING: The launch agent information isn't linked."
-            print "You can link it with:"
-            print "  %s" % link
+            print("WARNING: The launch agent information isn't linked.")
+            print("You can link it with:")
+            print("  %s" % link)
             return
         if os.path.islink(dest) and not os.readlink(dest) == src:
-            print "WARNING: '%s' isn't linked to '%s'."
-            print "You can link it with:"
-            print "  %s" % link
+            print("WARNING: '%s' isn't linked to '%s'.")
+            print("You can link it with:")
+            print("  %s" % link)
             return
-        print "The launch agent link seems to be fine."
-        print
+        print("The launch agent link seems to be fine.")
+        print()
 
 
 def main():
